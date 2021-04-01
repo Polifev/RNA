8a14c57638d47a2768be0ffa146ffdab6edbd7ee
Pol Lefèvre <pol007@skynet.be>
2021-04-01 07:56:05 +0200

    fix resources folder

--- src/main/java/be/hepl/rna/App.java
+++ src/main/java/be/hepl/rna/App.java
@@ -4,12 +4,12 @@ import be.hepl.rna.examples.MonoNeuronExamples;
 import be.hepl.rna.examples.MultilayerExamples;
 
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		MonoNeuronExamples.and();
-		MultilayerExamples.xor();
-		MultilayerExamples.twoClassesSeparation();
 		MonoNeuronExamples.table2_9();
 		MonoNeuronExamples.table2_10();
 		MonoNeuronExamples.table2_11();
+		MultilayerExamples.xor();
+		MultilayerExamples.twoClassesSeparation();
 	}
 }
