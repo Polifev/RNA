2341ee3e95056965da514348f6241fb6fe045bd7
Pol Lefèvre <pol007@skynet.be>
2021-04-22 14:15:12 +0200

    remove unused imports

--- src/test/java/be/hepl/rna/api/impl/matrix/TestMatrixFunctions.java
+++ src/test/java/be/hepl/rna/api/impl/matrix/TestMatrixFunctions.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import be.hepl.rna.api.impl.matrix.MatrixFunctions;
 import cern.colt.function.DoubleDoubleFunction;
 import cern.colt.function.DoubleFunction;
 
--- src/test/java/be/hepl/rna/api/impl/matrix/TestScalarMutliplication.java
+++ src/test/java/be/hepl/rna/api/impl/matrix/TestScalarMutliplication.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import be.hepl.rna.api.impl.matrix.ScalarMultiplication;
 import cern.colt.function.DoubleFunction;
 
 
